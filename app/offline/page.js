abd09246a7664be5a12ccb166414b5073a436722
Hiro <skinner.daniel2004@gmail.com>
2024-01-14 12:27:07 -0700

    PWA Support

---
+++ app/offline/page.js
@@ -0,0 +1,5 @@
+export default function Offline() {
+    return <div>
+        <h1>You are offline!</h1>
+    </div>
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+        removeConsole: process.env.NODE_ENV !== "development"
+    }
+}
 
-module.exports = nextConfig
+const withPWA = require("@ducanh2912/next-pwa").default({
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    dest: "public",
+    fallbacks: {
+        document: "/offline"
+    },
+    workboxOptions: {
+        disableDevLogs: true
+    }
+});
+
+module.exports = withPWA(nextConfig);
