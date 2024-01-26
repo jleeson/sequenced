fd8a53f071b73e80ee4e4fa727db8f323c310153
Hiro <skinner.daniel2004@gmail.com>
2024-01-25 19:30:54 -0700

    Nav Cleanup

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -3,12 +3,20 @@ import { Link } from "react-router-dom";
 
 export function NavSelector({ active, selectNav }) {
   return (
-    <div onClick={() => selectNav(!active)} className="flex bg-blue-400 w-10 h-10 justify-center items-center text-center rounded-full">
-      <div className={`${active ? "hidden" : "flex"} text-2xl text-gray-200`}>
-        +
-      </div>
-      <div className={`${active ? "flex" : "hidden"} text-2xl text-gray-200`}>
-        x
+    <div
+      onClick={() => selectNav(!active)}
+      className={`flex ${active ? "bg-blue-700" : "bg-green-700"} w-10 h-10 justify-center items-center text-center rounded-full`}
+    >
+      <div className={`flex text-2xl text-gray-200`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+          fill={`#FFFFFF`}
+        >
+          <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+        </svg>
       </div>
     </div>
   );
@@ -16,7 +24,7 @@ export function NavSelector({ active, selectNav }) {
 
 export function NavItem({ children, href }) {
   return (
-    <li className="flex justify-center border border-blue-600 px-4 py-2 bg-blue-400">
+    <li className="flex justify-center border border-blue-600 px-4 py-2 bg-blue-400 rounded-lg">
       <Link to={href} onClick={() => setActive(false)}>
         <div className="flex text-white">{children}</div>
       </Link>
@@ -27,7 +35,7 @@ export function NavItem({ children, href }) {
 export default function NavBar() {
   const [active, setActive] = useState(false);
 
-  function selectNav(activeState){
+  function selectNav(activeState) {
     setActive(activeState);
   }
 
@@ -39,8 +47,8 @@ export default function NavBar() {
         } flex-col-reverse absolute bottom-[125%] gap-2`}
       >
         <NavItem href="/">Home</NavItem>
-        <NavItem href="/meds">Meds</NavItem>
-        <NavItem href="/mood">Mood</NavItem>
+        {/* <NavItem href="/meds">Meds</NavItem> */}
+        {/* <NavItem href="/mood">Mood</NavItem> */}
         <NavItem href="/todo">To-Do</NavItem>
       </ul>
       <div className="w-full h-full flex flex-row justify-center items-center">
