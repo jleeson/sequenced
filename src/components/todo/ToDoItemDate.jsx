03c79cd923b9635900b89f469ddb457c248518e2
Hiro <skinner.daniel2004@gmail.com>
2024-03-13 16:08:18 -0600

    Overdue Status

--- src/components/todo/ToDoItemDate.jsx
+++ src/components/todo/ToDoItemDate.jsx
@@ -36,10 +36,17 @@ export default function ToDoItemDate({ date }) {
     }
   };
 
+  const isOverdue = (date) => {
+    const today = new Date();
+    if (new Date(date).getDate() < today.getDate()) return true;
+
+    return false;
+  };
+
   return (
-    <div className="flex flex-row gap-1 items-center">
+    <div className="w-full h-full flex flex-row gap-1 items-center">
       <img src={today_icon} className="invert w-4 h-4" width="16" height="16" />
-      <div>
+      <div className={`flex items-center w-12 h-6 ${isOverdue(date) ? "text-red-400" : "text-accent-white"}`}>
         <h1 className="text-lg">{checkRelative(date)}</h1>
       </div>
     </div>
