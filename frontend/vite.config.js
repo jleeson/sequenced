bff2d9cd90780d1e10204ac4850ec23d33938e37
Hiro <skinner.daniel2004@gmail.com>
2024-11-19 16:00:06 -0700

    Port remove dynamic

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { 
-    port: process.env.NODE_ENV == "development" ? 5173 : 8080
+    port: 8080
   },
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
   plugins: [react()],
