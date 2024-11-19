57c117a40335e700c869da48a3b887ab8825825f
Hiro <skinner.daniel2004@gmail.com>
2024-11-19 15:50:44 -0700

    Port Fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { 
-    port: 5173
+    port: process.env.NODE_ENV == "development" ? 5173 : 8080
   },
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
   plugins: [react()],
