3d9fc1429671ea4fc45a517a1d99219bf3081d05
Hiro <skinner.daniel2004@gmail.com>
2024-11-19 15:29:59 -0700

    Update Task Padding

--- frontend/src/components/task/TaskItemTitle.tsx
+++ frontend/src/components/task/TaskItemTitle.tsx
@@ -1,3 +1,3 @@
 export default function TaskItemTitle({ text }) {
-  return <div className="w-full text-lg mx-3 truncate">{text || "No Title"}</div>;
+  return <div className="w-full text-lg mx-3 truncate ml-2 mr-6">{text || "No Title"}</div>;
 }
