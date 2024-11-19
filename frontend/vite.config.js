646762ad088a70ca627aae4f2a7c491560811b94
Hiro <skinner.daniel2004@gmail.com>
2024-11-19 16:30:10 -0700

    Proper Dynamic Porting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
 
+import 'dotenv/config';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { 
-    port: 5173
+    port: process.env.VITE_PORT
   },
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
   plugins: [react()],
