f68af43848d2336d5d8d7a32fdd6b05de53a0363
Hiro <skinner.daniel2004@gmail.com>
2025-01-06 15:50:15 -0700

    Apple Tags

--- frontend/src/pages/(Home)/DueCapsule.tsx
+++ frontend/src/pages/(Home)/DueCapsule.tsx
@@ -1,6 +1,6 @@
 export default function DueCapsule({ count, category, important }) {
     return (
-        <div className="w-full h-full flex flex-col justify-end py-2 items-center shadow-md border border-accent-blue/80 p-2 relative rounded-md">
+        <div className="w-full h-full flex flex-col justify-end py-2 items-center shadow-md border border-accent-blue p-2 relative rounded-md">
             <div className="flex justify-center items-center w-full h-full">
                 <span className={`${important ? "text-5xl" : "text-3xl"} text-accent-blue`}>{count}</span>
             </div>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 import 'dotenv/config';
 
+const manifestForPlugIn = {
+  registerType: 'prompt',
+  includeAssests: ['favicon.ico', "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Sequenced: ADHD Management",
+    short_name: "Sequenced",
+    description: "Sequenced ADHD Todo App",
+    icons: [
+    {
+      src: '/assets/icons/icon-256.webp',
+      sizes: '512x512',
+      type: 'image/webp',
+      purpose: 'favicon'
+    },
+    {
+      src: '/assets/icons/icon-256.webp',
+      sizes: '180x180',
+      type: 'image/webp',
+      purpose: 'apple touch icon',
+    },
+    ],
+    theme_color: '#307acf',
+    background_color: '#FFFFFF',
+    display: "standalone",
+    scope: '/',
+    start_url: "/",
+    orientation: 'portrait'
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { 
+  server: {
     port: process.env.VITE_PORT
   },
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
 })
