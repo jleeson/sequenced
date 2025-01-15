1841dbb68a148e52c04948cad4d6b3d8fa8477ce
Hiro <skinner.daniel2004@gmail.com>
2025-01-14 22:05:04 -0700

    Clean Up Task Alignment

--- frontend/src/pages/(Home)/DueCapsule.tsx
+++ frontend/src/pages/(Home)/DueCapsule.tsx
@@ -4,7 +4,7 @@ export default function DueCapsule({ count, category, important }) {
             <div className="flex justify-center items-center w-full h-full">
                 <span className={`${important ? "text-5xl" : "text-3xl"} text-accent-blue`}>{count}</span>
             </div>
-            <span className="text-base text-gray-500">{count != 1 ? "Tasks" : "Task"} {category}</span>
+            <span className="text-base text-center text-gray-500">{count != 1 ? "Tasks" : "Task"} {category}</span>
         </div>
     )
 }
